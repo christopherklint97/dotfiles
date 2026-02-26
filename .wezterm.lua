f4ba69b81e83f6440e4b9879330d6b4214e45ba7
Christopher Klint <christopher.klint@gmail.com>
2026-02-26 16:45:03 +0100

    add more keybindings for tmuz, wezterm and zshrc

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,5 +11,15 @@ local config = wezterm.config_builder()
 config.color_scheme = "Dracula"
 config.font_size = 13.0
 
+-- Bypass tmux mouse reporting when CMD is held (enables Cmd+click on links)
+config.bypass_mouse_reporting_modifiers = "CMD"
+
+-- Keybindings
+config.keys = {
+  { key = "Backspace", mods = "CMD", action = wezterm.action.SendKey({ key = "u", mods = "CTRL" }) },
+  { key = "LeftArrow", mods = "CMD", action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }) },
+  { key = "RightArrow", mods = "CMD", action = wezterm.action.SendKey({ key = "e", mods = "CTRL" }) },
+}
+
 -- and finally, return the configuration to wezterm
 return config
